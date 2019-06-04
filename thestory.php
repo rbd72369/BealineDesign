750f7e46f9c08b426445ee6e2cf258ec07e297da
rbd72369 <rbd72369@uga.edu>
2019-06-04 12:19:26 -0400

    finlly got css working

--- thestory.php
+++ thestory.php
@@ -7,8 +7,20 @@
 
 </div>
 </header>
+<div id="story">
+	BeaLine Design was established in 1997 as a freelance design company. The concept of BeaLine was created from my name and which implies forward motion - it is a perfect metaphor becuase it embodies our belief that through creative and effective communication we can create competitive and lasting work for our clients.
 
-the story
+	BeaLine Design's purpose is to be flexible as a freelance design company. To design or produce creative and excellent work that will last for my clients needs.
+
+	Beatrice Patterson Sinco: <!--ask what degree--><br>
+	3 year<br>
+	communication and design<br>
+	9 years of experience at anno domini inc design studio<br>
+	25 years running BeaLine Design
+	<br>
+	Mission:<br>
+	BeaLine Design's mission is to create powerful graphic design solutions that generate great results for our clients.<br>
+</div>
 
 
 <?php
