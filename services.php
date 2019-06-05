8421130be3dfdd6fa14ff79093f9e2b8a53f1a73
rbd72369 <rbd72369@uga.edu>
2019-06-05 19:25:53 -0400

    css changes, added sidebee.png

--- services.php
+++ services.php
@@ -8,6 +8,10 @@
 </div>
 </header>
 
+<p>
+	BeaLine Design has a complete studio in house. We are flexible in working outside the studios to cater to our clients needs.
+</p>
+
 
 <?php
     include "bottom.html";
